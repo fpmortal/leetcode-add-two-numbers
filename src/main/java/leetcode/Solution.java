a246d2502252bb377c7500fac564046a7360e037
fpmortal <42036825+fpmortal@users.noreply.github.com>
2018-09-09 11:06:17 +0300

    solution refactoring

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -3,13 +3,9 @@ package leetcode;
 public class Solution {
 
     public ListNode addTwoNumbers(ListNode a, ListNode b) {
-        final ListNode c = new ListNode(a.val + b.val);
+        final ListNode head = new ListNode(0);
+        ListNode c = head;
 
-        ListNode c1 = c;
-        a = a.next;
-        b = b.next;
-
-        boolean dec = false;
         while (a != null || b != null) {
             if (a == null) {
                 a = new ListNode(0);
@@ -17,28 +13,22 @@ public class Solution {
                 b = new ListNode(0);
             }
 
-            if (c1.val >= 10) {
-                dec = true;
-                c1.val -= 10;
-            }
+            c.val += a.val + b.val;
 
-            c1.next = new ListNode(a.val + b.val);
-            c1 = c1.next;
-
-            if (dec) {
-                c1.val += 1;
-                dec = false;
+            if (c.val >= 10) {
+                c.val -= 10;
+                c.next = new ListNode(1);
+            } else {
+                if (a.next != null || b.next != null) {
+                    c.next = new ListNode(0);
+                }
             }
 
             a = a.next;
             b = b.next;
+            c = c.next;
         }
 
-        if (c1.val >= 10) {
-            c1.val -= 10;
-            c1.next = new ListNode(1);
-        }
-
-        return c;
+        return head;
     }
 }
